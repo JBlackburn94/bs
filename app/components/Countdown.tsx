709231eaa713c257e843a02dfdc76e965b26a9e1
Jason Blackburn <blackburnjason1994@gmail.com>
2025-07-17 11:42:00 +0100

    removes hardcoded apos with utf code

--- app/components/Countdown.tsx
+++ app/components/Countdown.tsx
@@ -30,7 +30,8 @@ export default function Countdown() {
     return () => clearInterval(timer);
   }, []);
 
-  if (!timeLeft) return <div className="text-xl font-semibold">Time's up!</div>;
+  if (!timeLeft)
+    return <div className="text-xl font-semibold">Time&apos;s up!</div>;
 
   return (
     <div className="flex justify-center items-center text-orange border-2 border-white/10 px-10 text-4xl font-semibold z-20 bg-white/10 backdrop-blur-md h-40 rounded-2xl shadow-orange shadow-lg/20">
