b3d9b01b6bce3a24b0c4802e2518a71cb3019078
Jason Blackburn <blackburnjason1994@gmail.com>
2026-02-05 18:49:00 +0000

    update hero content and ticket nav link href

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -12,7 +12,7 @@ export const links: LinkType[] = [
   },
   {
     name: "Tickets",
-    href: "https://www.ticketmaster.co.uk/beauty-school-tickets/artist/2269578",
+    href: "https://www.seetickets.com/tour/beauty-school?aff=id1google&src=googlead&inf=googlead&utm_source=google&utm_medium=cpc&utm_campaign=ar_Rock_tour_channable&utm_agid=186767315957&utm_term=beauty%20school%20tickets&creative=778300907943&device=c&placement=&gad_source=1&gad_campaignid=22745038233&gclid=Cj0KCQiAnJHMBhDAARIsABr7b84EJeVvKpVlJEr3_pu9-tQjZpKCYPx2FKlaXv_TQe0fNEjMJ0Y_oUEaAqBPEALw_wcB",
     target: "_blank",
   },
   {
